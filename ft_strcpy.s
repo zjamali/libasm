16ba2bbaade09a40d07964b379bb29246383f717
zjamali <zouhairjamali1998@gmail.com>
2021-01-24 10:45:59 +0100

    add ft_strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,14 +1,17 @@
 		global _ft_strcpy
-
+		extern _malloc
         section   .text
-_ft_strlen:   ;rax ft_strcp(rdi,rsi)
-			mov rbx , 0
-loop:
-			cmp [rdi + rbx] ,byte 0
+_ft_strcpy:   ;rax ft_strcp(rdi,rsi)
+			;push rdi
+			mov r8, 0
+_omay:
+			mov bl, [rsi + r8]
+			cmp bl , byte 0 
 			je ret
-			mov [rsi + rbx], [rdi + rbx]
-			inc rbx
-			jmp loop
+			mov [rdi + r8], bl
+			inc r8
+			jmp _omay
 ret:
-	mov rax , rsi
+	;pop rdi
+	mov rax , rdi
 	ret
--- main.c
+++ main.c
@@ -6,20 +6,20 @@
 /*   By: zjamali <zjamali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 08:44:56 by zjamali           #+#    #+#             */
-/*   Updated: 2021/01/23 19:07:10 by zjamali          ###   ########.fr       */
+/*   Updated: 2021/01/24 10:10:35 by zjamali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 size_t ft_strlen(char *str);
-
+char *ft_strcpy(char *dst,char *src);
 int main()
 {
 	char src[20] = "hello world";
 	char dst[20];
-	ft_strcpy(dst, src);	
+	char *result = ft_strcpy(dst, src);	
 	//int i = getinput();
-	printf("%zu",ft_strlen("hello mm"));
+	printf("%s",result);
 	return 0;
 }
